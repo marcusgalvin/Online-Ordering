675bedd3ff7d12dde8eb8ded294a38db4124781d
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-10-16 13:28:50 -0400

    fixed bug in api

--- keys.js
+++ keys.js
@@ -1,3 +1,5 @@
+//mongo db key, put in git ignore
 module.exports = {
-    mongoDBUrl: "mongodb+srv://marcus:careerdevs@cluster0-urkri.mongodb.net/test?retryWrites=true&w=majority"
-}
+  mongoDBUrl:
+    "mongodb+srv://marcus:careerdevs@cluster0-urkri.mongodb.net/test?retryWrites=true&w=majority"
+};
